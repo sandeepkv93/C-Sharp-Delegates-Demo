a820428b6f671b169d47a3e5c5d5009a36c7256a
Sandeep Vishnu <savishnu@microsoft.com>
2020-05-25 17:32:17 -0700

    Inital Commit | Delegates, Func and Actions Demo

---
+++ EntityModel/ShoppingCartModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityModel
+{
+    public class ShoppingCartModel
+    {
+        public delegate void MentionSubTotal(decimal subTotal);
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        public decimal GenerateTotal(MentionSubTotal mentionSubTotal,
+                                    Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal,
+                                    Action<string> tellUserWeAreDiscounting)
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            mentionSubTotal(subTotal);
+
+            tellUserWeAreDiscounting("We are applying your discount");
+
+            decimal total = calculateDiscountedTotal(Items, subTotal);
+            return total;
+        }
+    }
+}
